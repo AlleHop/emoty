040067decd0184b32160ec3bb83427aeeb12d337
AlleHop <dschmitt.1992@googlemail.com>
2021-09-16 17:00:55 +0200

    structure commands

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 require('dotenv').config(); //initialize dotenv
 const axios = require('axios'); //add this line at the top
-const {Client, Intents} = require('discord.js'); //import discord.js
+const Discord = require('discord.js'); //import discord.js
 
 async function getMeme(){
     const res = await axios.get('https://memeapi.pythonanywhere.com/');
     return res.data.memes[0].url;
 }
 
-const myIntents = new Intents();
-myIntents.add(Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS);
+const client = new Discord.Client({ intents: 513 }); //create new client
+client.commands = new Discord.Collection();
+const botCommands = require('./commands');
 
-const client = new Client({ intents: 14023 }); //create new client
-
+Object.keys(botCommands).map(key => {
+  client.commands.set(botCommands[key].name, botCommands[key]);
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLowerCase();
+  console.info(`Called command: ${command}`);
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(msg, args);
+  } catch (error) {
+    console.error(error);
+    msg.reply('there was an error trying to execute that command!');
+  }
   });
 
 //make sure this line is the last line
